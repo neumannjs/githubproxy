0b5dd75173ef7bcaf7139073e887e9017f06f06e
Gijs van Dam <gvandam@gmail.com>
2020-04-29 17:00:21 +0800

    Add flows for creating repos for new users

--- callback/index.js
+++ callback/index.js
@@ -1,25 +1,24 @@
 module.exports = async function (context, req) {
-    context.log('Proxy Github redirect')
+  context.log('Proxy Github redirect')
 
-    let state = decodeURI(req.query.state)
-    let indexOfHttp = state.search(/https?:\/\//i)
-
-    if (req.query.code && indexOfHttp > -1) {
-        let redirectUrl = state.substring(indexOfHttp) + '?code=' + encodeURI(req.query.code) + '&state=' + encodeURI(req.query.state)
-        context.res = {
-            status: 302,
-            headers: {
-                'location': redirectUrl
-            },
-            body: null
-        }
-        context.done()
+  const state = decodeURI(req.query.state)
+  const indexOfHttp = state.search(/https?:\/\//i)
+  // TODO: State should also contain a real state, to improve security
+  if (req.query.code && indexOfHttp > -1) {
+    const redirectUrl = state.substring(indexOfHttp) + '?code=' + encodeURI(req.query.code) + '&state=' + encodeURI(req.query.state)
+    context.res = {
+      status: 302,
+      headers: {
+        location: redirectUrl
+      },
+      body: null
     }
-    else {
-        context.res = {
-            status: 500,
-            body: "Incorrect request"
-        }
-        context.done()
+    context.done()
+  } else {
+    context.res = {
+      status: 500,
+      body: 'Incorrect request'
     }
-};
+    context.done()
+  }
+}
--- handler/index.js
+++ handler/index.js
@@ -1,16 +1,18 @@
+// Replace request module, becasue it is deprecated https://github.com/request/request/issues/3142
 const request = require('request')
 require('dotenv').config()
 
 const config = {
-  clientId: '93af288610e66a7a64a9',
+  clientId: process.env.CLIENTID,
   clientSecret: process.env.CLIENTSECRET,
-  redirectUri: 'http://localhost:7071/api/callback',
+  redirectUri: process.env.REDIRECT_URI,
   allowedOrigins: []
 }
 
-if (process.env.NODE_ENV === 'development') {
-  config.allowedOrigins.push('http://localhost:3000')
-}
+// if (process.env.NODE_ENV === 'development') {
+//   config.allowedOrigins.push('http://localhost:5500')
+//   config.allowedOrigins.push('http://localhost:3000')
+// }
 
 const handler = function (context) {
   context.log('Proxy Accescode request')
@@ -45,7 +47,7 @@ const handler = function (context) {
     const AuthorizedOptions = {
       headers: {
         'Content-Type': 'application/json',
-        Accept: 'application/vnd.github.mercy-preview+json',
+        Accept: 'application/vnd.github.mercy-preview+json', // custom media type to get topics in repository results
         'User-Agent': headers['user-agent'],
         Authorization: 'token ' + responseBody.access_token
       }
@@ -63,10 +65,15 @@ const handler = function (context) {
       const userGithubDomain = username + '.github.io'
 
       // for now we only allow people to edit their *own* repositories
+      // TODO: (Optional) Allow access to other repositories as well
       allowedOrigins.push('https://' + userGithubDomain)
+
+      let result = []
+      let personalGithubAvailable = false
 
       // Check for malicious request
       if (!allowedOrigins.includes(origin)) {
+
         // get the neumann repos if there are any
         // link to the create neumann repo if not
         request('https://api.github.com/user/repos', AuthorizedOptions, function (userReposErr, userInfoReposResponse) {
@@ -75,23 +82,124 @@ const handler = function (context) {
             return
           }
 
-          const neumannRepos = JSON.parse(userInfoReposResponse.body).filter(repo => repo.topics.includes('neumannssg'))
+          const responseJson = JSON.parse(userInfoReposResponse.body)
+          const neumannRepos = responseJson.filter(repo => { return repo.topics && repo.topics.includes('neumannssg') })
 
-          const result = neumannRepos.map(repo => ({
+          personalGithubAvailable = !responseJson.some(repo => repo.name === userGithubDomain)
+
+          result = neumannRepos.map(repo => ({
             name: repo.name,
-            url: userGithubDomain === repo.name ? 'https://' + userGithubDomain + '/admin/login' : 'https://' + userGithubDomain + '/' + repo.name + '/admin/login'
+            url: userGithubDomain === repo.name ? 'https://' + userGithubDomain + '/login' : 'https://' + userGithubDomain + '/' + repo.name + '/login'
           })
           )
+          console.log('RESULTS!')
+          console.log(result)
 
+          console.log('start repo create if needed')
+          // If reponame is provided, and the user doesn't already have neumannssg repos, create a new repo
+          if (context.req.query.reponame && context.req.query.reponame.length > 0 && result.length === 0) {
+            const AuthorizedOptionsWithForm = AuthorizedOptions
+            AuthorizedOptionsWithForm.form = JSON.stringify({
+              name: context.req.query.reponame
+            })
+
+            AuthorizedOptions.headers.Accept = 'application/vnd.github.baptiste-preview+json'
+
+            request.post('https://api.github.com/repos/' + process.env.TEMPLATE_REPO + '/generate', AuthorizedOptionsWithForm, function (forkErr, forkResponse) {
+              if (forkErr) {
+                context.log(forkErr)
+                context.done({ status: 500, error: forkErr })
+                return
+              }
+
+              if (forkResponse && forkResponse.statusCode === 201) {
+                // repo is created, now add topic (to be able to distinguish neumannssg repo's later on)
+                // PUT /repos/:owner/:repo/topics
+                const AuthorizedOptionsWithBody = AuthorizedOptions
+                AuthorizedOptionsWithBody.form = '{"names": ["neumannssg"]}'
+
+                AuthorizedOptionsWithBody.headers.Accept = 'application/vnd.github.mercy-preview+json'
+
+                context.log('Create topic')
+
+                request.put('https://api.github.com/repos/' + username + '/' + context.req.query.reponame + '/topics', AuthorizedOptionsWithBody, function (topicErr, topicResponse) {
+                  if (topicErr) {
+                    context.log('topic ERROR')
+                    context.log(topicErr)
+                    context.done({ status: 500, error: topicErr })
+                    return
+                  }
+                  context.log('topic response OK')
+                  context.log(topicResponse.body)
+
+                  const AuthorizedOptionsPages = AuthorizedOptions
+                  AuthorizedOptionsPages.form = JSON.stringify({
+                    source: {
+                      branch: 'master',
+                      path: '/docs'
+                    }
+                  })
+                  AuthorizedOptionsPages.headers.Accept = 'application/vnd.github.switcheroo-preview+json'
+
+                  context.log('Enable pages')
+
+                  request.post('https://api.github.com/repos/' + username + '/' + context.req.query.reponame + '/pages', AuthorizedOptionsPages, function (pagesErr, pagesResponse) {
+                    if (pagesErr) {
+                      context.log('pages ERROR')
+                      context.log(pagesErr)
+                      context.done({ status: 500, error: pagesErr })
+                      return
+                    }
+                    context.log('pages response OK')
+                    context.log(pagesResponse.statusCode)
+                    context.log(pagesResponse.body)
+
+                    // Sent back the an authentication error but with the newly created repository as info
+                    result = [{
+                      name: context.req.query.reponame,
+                      url: userGithubDomain === context.req.query.reponame ? 'https://' + userGithubDomain + '/login' : 'https://' + userGithubDomain + '/' + context.req.query.reponame + '/login'
+                    }]
+
+                    console.log('context done!')
+                    console.log(result)
+                    const error = `${origin} is not an allowed origin.`
+                    context.res = {
+                      status: 401,
+                      body: {
+                        error: error,
+                        info: JSON.stringify(result)
+                      }
+                    }
+                    context.done()
+                  })
+                })
+              } else {
+                context.res = {
+                  status: 500,
+                  body: {
+                    error: JSON.parse(forkResponse.body)
+                  }
+                }
+              }
+            })
+          }
+
+          if (result.length === 0 && personalGithubAvailable) {
+            result = [{
+              create: userGithubDomain
+            }]
+          }
+
+          console.log('context done!')
+          console.log(result)
           const error = `${origin} is not an allowed origin.`
           context.res = {
             status: 401,
             body: {
               error: error,
-              info: result
+              info: JSON.stringify(result)
             }
           }
-
           context.done()
         })
       } else {
@@ -104,6 +212,7 @@ const handler = function (context) {
 
         context.done()
       }
+
     })
   })
 }
